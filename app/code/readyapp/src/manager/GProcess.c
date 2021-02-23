8a67322110bc034e63aa794b690cb43edbb662d5
Gerard KESSE <tiakagerard@hotmail.com>
2021-02-23 20:59:49 +0100

    Initial Commit

--- app/code/readyapp/src/manager/GProcess.c
+++ app/code/readyapp/src/manager/GProcess.c
@@ -9,7 +9,7 @@ void GProcess_Run(eGProcess key) {
 }
 //================================================
 void GProcess_Mini_Blink() {
-	GMiniBlink_Init();
-	xTaskCreate(GMiniBlink_Update, "LED", 100, NULL, configMAX_PRIORITIES-1, NULL);
+    GMiniBlink_Init();
+    xTaskCreate(GMiniBlink_Update, "LED", 100, NULL, configMAX_PRIORITIES-1, NULL);
 }
 //================================================
